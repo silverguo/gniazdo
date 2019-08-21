15a85a7bce22a83572705d652d4fee3b079e4697
Yuhan Guo <yuhan@liftoff.io>
2019-08-21 15:58:24 +0200

    Version 1.1.3

--- project.clj
+++ project.clj
@@ -1,11 +1,6 @@
-(defproject stylefruits/gniazdo "1.1.2"
-  :description "A WebSocket client for Clojure"
-  :license {:name "Apache License 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0.html"
-            :year 2013
-            :key "apache-2.0"
-            :author "stylefruits GmbH"}
-  :url "https://github.com/stylefruits/gniazdo"
+(defproject org.clojars.silverguo/gniazdo-fork "1.1.3"
+  :description "Fork of webSocket client for Clojure from stylefruits/gniazdo"
+  :url "https://github.com/silverguo/gniazdo"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.eclipse.jetty.websocket/websocket-client "9.4.19.v20190610"]]
   :repl-options {:init-ns gniazdo.core}
